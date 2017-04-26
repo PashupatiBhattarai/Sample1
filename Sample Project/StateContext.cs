ea14010ade3b6bd4a68643a3ff662758ec8b63b8
Pashupatibhattarai <itspashupati@yahoo.com>
2017-04-25 20:04:36 -0600

    Add project files.

---
+++ Sample Project/StateContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Sample_Project
+{
+   public class StateContext:DbContext
+    {
+        public DbSet<States> State { get; set; }
+    }
+}
---
+++ Sample Project/States.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Project
+{
+    public class States
+    {
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+        public string  City { get; set; }
+        public DateTime Modifieddate { get; set; }
+
+
+    }
+}
